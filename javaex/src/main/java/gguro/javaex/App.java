39371d46d20816c0f0961b11c445787a13df836e
2-4 <2-4@192.168.0.2>
2017-07-21 12:23:13 +0900

    let's commit

--- javaex/src/main/java/gguro/javaex/App.java
+++ javaex/src/main/java/gguro/javaex/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Github!" );
+        System.out.println( "Let's commit" );
         
     }
 }
