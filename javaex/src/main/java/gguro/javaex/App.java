158c1760e5ac95f135dcad9534730009be5542a0
2-4 <2-4@192.168.0.2>
2017-07-21 12:20:38 +0900

    hello github

---
+++ javaex/src/main/java/gguro/javaex/App.java
@@ -0,0 +1,14 @@
+package gguro.javaex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Github!" );
+        
+    }
+}
